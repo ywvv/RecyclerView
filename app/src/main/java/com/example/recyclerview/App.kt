623c7a35638b92027c44db6a2791aab30dabbc26
Vlad Horban <ywvvvw@gmail.com>
2023-05-04 15:46:56 +0300

    add activity_main

---
+++ app/src/main/java/com/example/recyclerview/App.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerview
+
+import android.app.Application
+import com.example.recyclerview.model.UsersService
+
+class App: Application() {
+
+    val usersService: UsersService()
+}
