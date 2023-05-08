ea3c1d9fcb9a6ae683e67d6d17a4a342ef19f914
Vlad Horban <ywvvvw@gmail.com>
2023-05-08 13:23:05 +0300

    add popup

--- app/src/main/java/com/example/recyclerview/UsersAdapter.kt
+++ app/src/main/java/com/example/recyclerview/UsersAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.recyclerview
 
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerview.databinding.ItemUserBinding
@@ -59,15 +61,58 @@ class UsersAdapter(private val actionListener: UserActionListener) :
         }
     }
 
+    private fun showPopupMenu(view: View) {
+        val context = view.context
+        val user = view.tag as User
+        val position = users.indexOfFirst { it.id == user.id }
+        val popupMenu = PopupMenu(context, view)
+
+        popupMenu.menu.add(0, ID_MOVE_UP, Menu.NONE, context.getString(R.string.move_up))
+            .apply {
+                isEnabled = position > 0
+            }
+        popupMenu.menu.add(0, ID_MOVE_DOWN, Menu.NONE, context.getString(R.string.move_down))
+            .apply {
+                isEnabled = position < users.size - 1
+            }
+        popupMenu.menu.add(0, ID_REMOVE, Menu.NONE, context.getString(R.string.remove))
+
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                ID_MOVE_UP -> {
+                    actionListener.onUserMove(user, -1)
+                }
+
+                ID_MOVE_DOWN -> {
+                    actionListener.onUserMove(user, 1)
+                }
+
+                ID_REMOVE -> {
+                    actionListener.onUserDelete(user)
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+        popupMenu.show()
+    }
+
     override fun onClick(view: View) {
         val user = view.tag as User
         when (view.id) {
             R.id.moreImageViewButton -> {
-                TODO()
+                showPopupMenu(view)
             }
+
             else -> {
                 actionListener.onUserDetails(user)
             }
         }
+    }
+
+    companion object {
+        private const val ID_MOVE_UP = 1
+        private const val ID_MOVE_DOWN = 2
+        private const val ID_REMOVE = 3
     }
 }
